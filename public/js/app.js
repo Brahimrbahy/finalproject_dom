3a2cfe2d3afbc2bc3fc04754002a11225e2b02f6
Brahimrbahy <brahim.rbahy@gmail.com>
2025-05-29 12:57:33 +0100

    menus2

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,4 @@
-// let modal = document.getElementById('idmod');
-// let loginBtn = document.getElementById('loginBtn');
-// let closeBtn = document.getElementById('closeBtn');
 
-
-// loginBtn.addEventListener('click', () => {
-//   modal.style.display = 'block';
-// });
-
-// closeBtn.addEventListener('click', () => {
-//   modal.style.display = 'none';
-// });
-
-
-
-// window.addEventListener('click', (event) => {
-//   if (event.target === modal) {
-//     modal.style.display = 'none';
-//   }
-// });
 
 
 
@@ -26,7 +7,7 @@ let myModals = document.querySelectorAll(".backdrop");
 
 myButtons.forEach(btn => {
     btn.addEventListener('click', (e) => {
-        e.preventDefault(); // prevent <a href="">
+        e.preventDefault(); 
         let modalData = btn.getAttribute("modal-data");
         let modal = document.getElementById(modalData);
         if (modal) {
@@ -50,3 +31,6 @@ myModals.forEach(modal => {
         }
     });
 });
+
+
+
